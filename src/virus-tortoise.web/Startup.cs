c0ab439c2d8db53f7a0613c11a0d7f0eb17b4c8f
Jarred Capellman <jcapellman@hotmail.com>
2020-02-04 18:55:46 -0600

    Change: Removed un-used namespace references

--- src/virus-tortoise.web/Startup.cs
+++ src/virus-tortoise.web/Startup.cs
@@ -1,13 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
+
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+
 using virus_tortoise.web.Data;
 
 namespace virus_tortoise.web
@@ -21,8 +18,6 @@ namespace virus_tortoise.web
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
@@ -30,7 +25,6 @@ namespace virus_tortoise.web
             services.AddSingleton<WeatherForecastService>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
