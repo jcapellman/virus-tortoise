392873db6c62cbed01c4b4fdd3cacdeee3a734be
Jarred Capellman <jcapellman@hotmail.com>
2020-02-05 20:21:48 -0600

    Added: Hashing Extension Class

---
+++ src/virus-tortoise.lib/Extensions/Hashing.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace virus_tortoise.lib.Extensions
+{
+    public static class Hashing
+    {
+        public static string ToSHA256(this byte[] data)
+        {
+            using (var sha = new SHA256Managed())
+            {
+                return string.Concat(sha.ComputeHash(data).Select(item => 
+                    item.ToString("x2", CultureInfo.InvariantCulture))).ToUpper(CultureInfo.InvariantCulture);
+            }
+        }
+    }
+}
