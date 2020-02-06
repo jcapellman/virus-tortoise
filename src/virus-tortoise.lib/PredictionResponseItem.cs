f4f5d07a3ae37e4ff93772a7700d8ce079ba1257
Jarred Capellman <jcapellman@hotmail.com>
2020-02-05 20:22:21 -0600

    Change: SHA1 is now SHA256 in the PredictionResponseItem class

--- src/virus-tortoise.lib/PredictionResponseItem.cs
+++ src/virus-tortoise.lib/PredictionResponseItem.cs
@@ -2,7 +2,7 @@ namespace virus_tortoise.lib
 {
     public class PredictionResponseItem
     {
-        public string SHA1 { get; set; }
+        public string SHA256 { get; set; }
 
         public long FileSize { get; set; }
 
