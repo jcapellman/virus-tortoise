cbd80ad28e1ed1caddf540ef67be4c6403b88a2b
Jarred Capellman <jcapellman@hotmail.com>
2020-02-06 19:55:52 -0600

    Added: MD5 and SHA1 Hashing Algorithms to the Extensions class

--- src/virus-tortoise.lib/Extensions/Hashing.cs
+++ src/virus-tortoise.lib/Extensions/Hashing.cs
@@ -14,5 +14,23 @@ namespace virus_tortoise.lib.Extensions
                     item.ToString("x2", CultureInfo.InvariantCulture))).ToUpper(CultureInfo.InvariantCulture);
             }
         }
+
+        public static string ToSHA1(this byte[] data)
+        {
+            using (var sha = new SHA1Managed())
+            {
+                return string.Concat(sha.ComputeHash(data).Select(item =>
+                    item.ToString("x2", CultureInfo.InvariantCulture))).ToUpper(CultureInfo.InvariantCulture);
+            }
+        }
+
+        public static string ToMD5(this byte[] data)
+        {
+            using (var md5 = MD5.Create())
+            {
+                return string.Concat(md5.ComputeHash(data).Select(item =>
+                    item.ToString("x2", CultureInfo.InvariantCulture))).ToUpper(CultureInfo.InvariantCulture);
+            }
+        }
     }
 }
