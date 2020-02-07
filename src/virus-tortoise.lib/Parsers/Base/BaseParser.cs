592d45a4c112f331ac318bb14936ada481321aa7
Jarred Capellman <jcapellman@hotmail.com>
2020-02-06 21:25:14 -0600

    Added: BaseParser class

---
+++ src/virus-tortoise.lib/Parsers/Base/BaseParser.cs
@@ -0,0 +1,9 @@
+﻿namespace virus_tortoise.lib.Parsers.Base
+{
+    public abstract class BaseParser
+    {
+        public abstract string FileType { get; }
+
+        public abstract bool IsFile(byte[] data);
+    }
+}
