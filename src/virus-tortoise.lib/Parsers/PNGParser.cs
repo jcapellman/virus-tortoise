d403fc5f85e169930af1b8a3fc4670227fa0e9de
Jarred Capellman <jcapellman@hotmail.com>
2020-02-06 21:26:05 -0600

    Added: PNGParser class

---
+++ src/virus-tortoise.lib/Parsers/PNGParser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using virus_tortoise.lib.Parsers.Base;
+
+namespace virus_tortoise.lib.Parsers
+{
+    public class PNGParser : BaseParser
+    {
+        private byte[] FileMagicBytes = new byte[] { 137, 80, 78, 71, 13, 10, 26, 10 };
+
+        public override string FileType => "PNG";
+
+        public override bool IsFile(byte[] data) => data.AsSpan().Slice(0, FileMagicBytes.Length).SequenceEqual(FileMagicBytes);
+    }
+}
