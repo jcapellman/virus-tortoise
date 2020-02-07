250387609618f797fc4ff078aff061c81e98ca88
Jarred Capellman <jcapellman@hotmail.com>
2020-02-06 21:24:06 -0600

    Added: Parser Loading in the PredictionService class

--- src/virus-tortoise.lib/PredictionService.cs
+++ src/virus-tortoise.lib/PredictionService.cs
@@ -1,21 +1,44 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+
 using virus_tortoise.lib.Extensions;
+using virus_tortoise.lib.Parsers.Base;
 
 namespace virus_tortoise.lib
 {
     public class PredictionService
     {
+        private static List<BaseParser> _parsers;
+
+        public PredictionService()
+        {
+            if (_parsers == null)
+            {
+                _parsers = typeof(BaseParser).Assembly.GetTypes().Where(a => a.BaseType == typeof(BaseParser) && !a.IsAbstract).Select(b => (BaseParser)Activator.CreateInstance(b)).ToList();
+            }
+        }
+
         public PredictionResponseItem PredictFile(byte[] fileData)
         {
-            return new PredictionResponseItem
+            var response = new PredictionResponseItem
             {
                 FileSize = fileData.Length,
                 SHA256 = fileData.ToSHA256(),
                 SHA1 = fileData.ToSHA1(),
                 MD5 = fileData.ToMD5(),
-                FileType = "Unknown",
+                FileType = "Unsupported File Type",
                 ErrorMessage = string.Empty
             };
+
+            var parser = _parsers.FirstOrDefault(a => a.IsFile(fileData));
+
+            if (parser != null)
+            {
+                response.FileType = parser.FileType;
+            }
+
+            return response;
         }
     }
 }
