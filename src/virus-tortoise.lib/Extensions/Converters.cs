6b5a9486c98097a482930fb7243def8674436583
Jarred Capellman <jcapellman@hotmail.com>
2020-02-09 08:57:11 -0600

    Added: Non-starting index support in the Converters class

---
+++ src/virus-tortoise.lib/Extensions/Converters.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+namespace virus_tortoise.lib.Extensions
+{
+    public static class Converters
+    {
+        public static int ToInt32(this byte[] data, int startIndex = 0)
+        {
+            data = data.Skip(startIndex).Take(4).Reverse().ToArray();
+
+            int result = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                result = data[0] | (data[1] << 8) | (data[2] << 16) | (data[3] << 24);
+            }
+         
+            return result;            
+        }
+    }
+}
